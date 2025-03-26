8f0ab5b0efbf7460e47f8e3346a9a05bb4643cb1
Nicolas Agudelo <nicolas.agudelog@autonoma.edu.co>
2025-03-25 20:33:34 -0500

    clases aplicación y amigo creadas

--- src/aplicacionAmigos/models/Amigo.java
+++ src/aplicacionAmigos/models/Amigo.java
@@ -3,4 +3,31 @@ package aplicacionAmigos.models;
 // @author vanes
 public class Amigo {
 
+    private String nombre; 
+    private String telefono; 
+    private String correoElectronico; 
+
+    
+    public Amigo(String nombre, String telefono, String correoElectronico) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+    }
+
+    
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+    
+    
 }
--- src/aplicacionAmigos/models/Aplicacion.java
+++ src/aplicacionAmigos/models/Aplicacion.java
@@ -1,6 +1,36 @@
 package aplicacionAmigos.models;
-
+import java.util.ArrayList; 
 // @author vanes
 public class Aplicacion {
-
+    
+    ArrayList<Amigo> amigos = new ArrayList<Amigo>();
+    
+    public boolean agregarAmigo(Amigo amigoNuevo){
+        for(int i=0 ; i<amigos.size(); i++){
+            if(amigoNuevo == amigos.get(i)){
+                return false; 
+            }
+        }
+        return true; 
+    }
+    
+    public Amigo buscarAmigo(String correoElectronicoABuscar) {
+        for(int i=0 ; i<amigos.size(); i++){
+            String correo = amigos.get(i).getCorreoElectronico();
+            if (correo == correoElectronicoABuscar){
+                return amigos.get(i);
+            }
+        }
+        return null; 
+    }
+    
+    public void EliminarAmigo(String correoElectronicoABuscar){
+         for(int i=0 ; i<amigos.size(); i++){
+            String correo = amigos.get(i).getCorreoElectronico();
+            if (correo == correoElectronicoABuscar){
+                amigos.remove(i);
+            }
+        }
+    }
+    
 }
